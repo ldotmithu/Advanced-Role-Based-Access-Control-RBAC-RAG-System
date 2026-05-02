881210e16fbf3cb2788b220cff2b7a3d4cc57a8b
ldotmithu <ldotmithurshan222@gmail.com>
2026-05-02 17:43:16 +0530

    install all library

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup,find_packages
+setup(
+    name="RBAC-RAG-Appliaction",
+    version="0.1.0",
+    author="mithurshan",
+    author_email="ldotmiturshan222@gamil.com",
+    find_packages = find_packages()
+)
