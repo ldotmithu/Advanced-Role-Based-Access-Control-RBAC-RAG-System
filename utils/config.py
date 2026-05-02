a496ac564703472f5589c89d745303b5b9898bc9
ldotmithu <ldotmithurshan222@gmail.com>
2026-05-02 22:23:31 +0530

    update model

--- utils/config.py
+++ utils/config.py
@@ -2,7 +2,7 @@ import os
 
 class settings:
     GROQ_API_KEY = os.getenv("GROQ_API_KEY")
-    LLM_MODEL = os.getenv("LLM_MODEL",default="openai/gpt-oss-20b")
+    LLM_MODEL = os.getenv("LLM_MODEL")
     EMBEDDING_MODEL = os.getenv("EMBEDDING_MODEL",default="sentence-transformers/all-MiniLM-L6-v2")
     CHUNK_SIZE = 1100
     CHUNK_OVERLAP = 450
